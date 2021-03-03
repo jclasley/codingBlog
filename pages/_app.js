dac5e6f46199cb3256245e8aa6412b14e9b82f27
jon.lasley@gmail.com <jon.lasley@gmail.com>
2021-03-03 13:15:52 -0700

    Removed GTM and am importing the tag directly to test

--- pages/_app.js
+++ pages/_app.js
@@ -4,12 +4,12 @@ import { Router } from 'next/router';
 import {useEffect} from 'react';
 import '../styles/prism.css';
 export default function App({ Component, pageProps }) {
-  useEffect(() => {
-    const handleChangeRoute = (url) => GTMPageView(url);
-    Router.events.on('routeChangeComplete', handleChangeRoute);
-    return () => {
-      Router.events.off('routeChangeComplete', handleChangeRoute);
-    };
-  }, [])
+  // useEffect(() => {
+  //   const handleChangeRoute = (url) => GTMPageView(url);
+  //   Router.events.on('routeChangeComplete', handleChangeRoute);
+  //   return () => {
+  //     Router.events.off('routeChangeComplete', handleChangeRoute);
+  //   };
+  // }, [])
   return <Component {...pageProps} />;
 }
--- pages/_document.js
+++ pages/_document.js
@@ -11,26 +11,22 @@ class MyDoc extends Document {
       <Html>
         <Head>
           {/* <!-- Google Tag Manager --> */}
+          <script async src="https://www.googletagmanager.com/gtag/js?id=UA-163152822-1"></script>
+
           <script dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l] = w[l] || []{'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-5CXHDTB');`}}></script>
+            __html: `window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-163152822-1');`}}></script>
           {/* <!-- End Google Tag Manager --> */}
-          <script dangerouslySetInnerHTML={{
-            __html: 'console.log("hello")'
-          }}></script>
+          
           {/* Google fonts */}
           <link href="https://fonts.googleapis.com/css2?family=Zilla+Slab:wght@500&display=swap" rel="stylesheet"></link>
           <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@1,100&display=swap" rel="stylesheet"></link>
           <link href="https://fonts.googleapis.com/css2?family=Amatic+SC:wght@400;700&family=Courgette&family=Lobster&display=swap" rel="stylesheet"></link>
         </Head>
         <body>
-          {/* <!-- Google Tag Manager (noscript) --> */}
-          <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5CXHDTB"
-            height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-          {/* <!-- End Google Tag Manager (noscript) --> */}
 
          
           <Main />
