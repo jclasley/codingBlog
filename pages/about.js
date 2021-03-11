8671b29246a0494925ce994e7713059312022f3d
jon.lasley@gmail.com <jon.lasley@gmail.com>
2021-03-11 13:25:41 -0700

    Formatting

--- pages/about.js
+++ pages/about.js
@@ -1,29 +1,50 @@
 import Head from 'next/head'
 import Link from 'next/link'
 import Layout from '../components/layout'
+import Header from '../components/header'
+import Linkbar from '../components/Linkbar';
 
 export default function About() {
   return (
     <>
-      <Layout>
         <Head>
           <title>About me</title>
         </Head>
-        <h1>
-          About Jon
-        </h1>
-        <div>
-          Hello and welcome! My name is Jon Lasley, and I'm a software engineer who loves solving problems.
-          My love for software engineering started as a desire to automate my mundane job functions in VBA while in my previous career, turned into a passion project in C#, and is now a full-blown love of all things software.
-          <br />
-          <br />
-          I am overjoyed at the opportunity to learn as much as possible and grow as an engineer. My current language focuses are in JavaScript, Clojure, and Go. I am having a blast learning Clojure and Go, as they are very different from my most mastered language, JavaScript.
-          <br />
-          I love to find clean and beautiful solutions to difficult and messy problems. Learning and growing as an engineer is what keeps me coming back day after day and what fuels my passion.
-          <br />
-          <br />
-          This site started as a blog to detail my experience through a software engineering immersive, but has transitioned into a learning and educational platform, focused mainly on my <a href='/algo'>Algo a Day series</a>. It's written using next.js, and I've thoroughly enjoyed the experience of creating a site using it, as compared to the messiness that is the traditional React, Express, and Webpack stack! Check back later to see my progress.
-        </div>
+        <Header title="About" />
+        <Linkbar links={[
+          {
+            src: '/algo',
+            name: 'Algo a Day'
+          },
+          {
+            src: '/projects',
+            name: 'Projects'
+          },
+          {
+            src: 'https://www.github.com/jclasley',
+            name: 'Github'
+          },
+          {
+            src: '/about',
+            name: 'About'
+          }
+        ]} />
+        <Layout>
+          <h1>
+            About Jon
+          </h1>
+          <div>
+            Hello and welcome! My name is Jon Lasley, and I'm a software engineer who loves solving problems.
+            My love for software engineering started as a desire to automate my mundane job functions in VBA while in my previous career, turned into a passion project in C#, and is now a full-blown love of all things software.
+            <br />
+            <br />
+            I am overjoyed at the opportunity to learn as much as possible and grow as an engineer. My current language focuses are in JavaScript, Clojure, and Go. I am having a blast learning Clojure and Go, as they are very different from my most mastered language, JavaScript.
+            <br />
+            I love to find clean and beautiful solutions to difficult and messy problems. Learning and growing as an engineer is what keeps me coming back day after day and what fuels my passion.
+            <br />
+            <br />
+            This site started as a blog to detail my experience through a software engineering immersive, but has transitioned into a learning and educational platform, focused mainly on my <a href='/algo'>Algo a Day series</a>. It's written using next.js, and I've thoroughly enjoyed the experience of creating a site using it, as compared to the messiness that is the traditional React, Express, and Webpack stack! Check back later to see my progress.
+          </div>
       </Layout>
     </>
         
--- pages/projects.js
+++ pages/projects.js
@@ -1,29 +1,50 @@
 import Head from 'next/head'
 import Layout from '../components/layout'
 import Link from 'next/link'
+import Header from '../components/header'
+import Linkbar from '../components/Linkbar';
 
 export default function Projects() {
   return (
     <>
-      <Layout>
         <Head>
           <title>Projects</title>
         </Head>
-        <h1>
-          My side-projects
-        </h1>
-        <h2><Link href='https://awesomeasteroids.herokuapp.com'><a>Awesome Asteroids</a></Link></h2>
-        <div>Built on the MERN stack, this website utilizes NASA's near-earth-object API to retrieve and store information about the asteroid's nearest earth for a given date. The size and distance of the asteroids are logarithmically proportional to the true data, and an asteroid's orbital period is linearly proportional. Check out zen mode for the ultimate immersive experience! It's deployed on Heroku's free tier, so I apologize about initial load times. 
-        </div>
-        <h2><Link href='https://github.com/jclasley/LoveQuick'><a>LoveQuick</a></Link></h2>
-        <div>The one that started it all. A native iOS app designed for staying in touch with your friends and family
-        with minimal effort. Just a quick way to drop a line and say you're thinking about them.
-        </div>
-        <h2><Link href='https://github.com/jclasley/ReactiveLevel'><a>Reactive level</a></Link></h2>
-        <div>
-          Built as a way to learn RxSwift and the reactive programming paradigm, the reactive level is simply that -- a level
-           that displays offset on two axes based on device attitude. 
-        </div>
+        <Header title="Projects" />
+        <Linkbar links={[
+          {
+            src: '/algo',
+            name: 'Algo a Day'
+          },
+          {
+            src: '/projects',
+            name: 'Projects'
+          },
+          {
+            src: 'https://www.github.com/jclasley',
+            name: 'Github'
+          },
+          {
+            src: '/about',
+            name: 'About'
+          }
+        ]} />
+        <Layout>
+          <h1>
+            My side-projects
+          </h1>
+          <h2><Link href='https://awesomeasteroids.herokuapp.com'><a>Awesome Asteroids</a></Link></h2>
+          <div>Built on the MERN stack, this website utilizes NASA's near-earth-object API to retrieve and store information about the asteroid's nearest earth for a given date. The size and distance of the asteroids are logarithmically proportional to the true data, and an asteroid's orbital period is linearly proportional. Check out zen mode for the ultimate immersive experience! It's deployed on Heroku's free tier, so I apologize about initial load times. 
+          </div>
+          <h2><Link href='https://github.com/jclasley/LoveQuick'><a>LoveQuick</a></Link></h2>
+          <div>The one that started it all. A native iOS app designed for staying in touch with your friends and family
+          with minimal effort. Just a quick way to drop a line and say you're thinking about them.
+          </div>
+          <h2><Link href='https://github.com/jclasley/ReactiveLevel'><a>Reactive level</a></Link></h2>
+          <div>
+            Built as a way to learn RxSwift and the reactive programming paradigm, the reactive level is simply that -- a level
+            that displays offset on two axes based on device attitude. 
+          </div>
       </Layout>
     </>
         
